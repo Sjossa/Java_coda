f3f9f0733ee6166ea800fc639a3f8a1da8c3d11c
Johnny Sassiat <johnny.sass2001@gmail.com>
2026-01-05 11:00:12 +0100

    Java_initilaize

---
+++ Cours/J1/first.java
@@ -0,0 +1,11 @@
+//fonctionnement = java <nom du fichier>
+package J1;
+public class first{
+
+    public static  void  main(String[] args){
+        System.out.println("bonjour le monde");
+
+        //declaration d'une variable type puis nom 
+        int nombre;
+    }
+}
